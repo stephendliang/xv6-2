7b620548b2d5ddee0025716ccc2d7c3e5c4bc894
Stephen Liang <sl@sent.at>
2022-10-04 13:51:22 -0700

    again

--- defs.h
+++ defs.h
@@ -117,6 +117,15 @@ void            wakeup(void*);
 void            yield(void);
 void 			killSelf(void);
 
+int kthread_create(void* (*start_func)(), void* stack, int stack_size);
+int kthread_id();
+void kthread_exit();
+int kthread_join(int thread_id);
+int kthread_mutex_alloc();
+int kthread_mutex_dealloc(int mutex_id);
+int kthread_mutex_lock(int mutex_id);
+int kthread_mutex_unlock(int mutex_id);
+
 // swtch.S
 void            swtch(struct context**, struct context*);
 
